53a8b9e5e980cff043b39c1fe86495eecb481c46
unknown <lprieu@gamingcampus.local>
2022-03-03 20:42:35 +0100

    add content

--- react-app/src/Accueil.js
+++ react-app/src/Accueil.js
@@ -1,34 +1,100 @@
 import React, { Component } from 'react';
-import {Carousel, Col, Container} from 'react-bootstrap'
+import { Card, CardGroup, Carousel, Col, Container, Row } from 'react-bootstrap'
 import NavSite from './Navbar';
 
 class Accueil extends Component {
 
-    render() {
+  render() {
     return (
       <>
-      <NavSite/>
-        <Carousel>
-          <Carousel.Item interval={2000}>
+        <NavSite />
+        <Carousel className=''>
+          <Carousel.Item>
             <img
               className="d-block w-100"
-              src="http://localhost:1337/uploads/992x595_Carrousel_mobile_Muta_8a06328523.jpg?updated_at=2022-03-02T17:13:07.894Z"
+              src="http://localhost:1337/uploads/tofu_royal_f2cda04568.JPG?updated_at=2022-03-03T18:10:43.717Z"
               alt="First slide"
             />
           </Carousel.Item>
-          <Carousel.Item interval={2000}>
+          <Carousel.Item>
             <img
               className="d-block w-100"
-              src="http://localhost:1337/uploads/992x595_FR_Carrousel_Tapis_IOP_Retro_min_ad6a10594d.jpg?updated_at=2022-03-02T17:13:07.875Z"
+              src="http://localhost:1337/uploads/unfollow_455f256cb1.JPG?updated_at=2022-03-03T18:10:43.730Z"
               alt="Second slide"
             />
           </Carousel.Item>
-          <Carousel.Item interval={2000}>
+          <Carousel.Item>
             <img
               className="d-block w-100"
-              src="http://localhost:1337/uploads/Carrousel_responsive_Peluche_Tofu_Royal_FR_c433cfc89c.jpg?updated_at=2022-03-02T17:13:07.874Z"
+              src="http://localhost:1337/uploads/mutafukaz_a2c922a54a.JPG?updated_at=2022-03-03T18:06:13.938Z"
               alt="Third slide"
             />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="http://localhost:1337/uploads/tapis_souris_iop_159fa5ac8e.JPG?updated_at=2022-03-03T18:10:43.718Z"
+              alt="Fourth slide"
+            />
+          </Carousel.Item>
+        </Carousel>
+        <Container>
+          <Row>
+            <Col>
+              <h2 className="text-uppercase mt-4">nouveautés</h2>
+            </Col>
+          </Row>
+        </Container>
+        <Carousel variant="dark">
+          <Carousel.Item>
+            <CardGroup>
+              <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+              </Card>
+              <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    This card has supporting text below as a natural lead-in to additional
+                    content.{' '}
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+              </Card>
+              <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This card has even longer content than the first to
+                    show that equal height action.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+              </Card>
+            </CardGroup>
+          </Carousel.Item>
+          <Carousel.Item>
+
+          </Carousel.Item>
+          <Carousel.Item>
+
           </Carousel.Item>
         </Carousel>
       </>
